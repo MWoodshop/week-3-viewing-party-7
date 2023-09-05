1ae7999da377c6c3df21918abd0639ebfc63c2bd
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-05 16:59:03 -0400

    Test: adds sad path test for user story 4 login

--- spec/features/login_page_spec.rb
+++ spec/features/login_page_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe 'Logging In' do
     expect(current_path).to eq(user_path(user.id))
     expect(page).to have_content("Welcome, #{user.name}!")
   end
+
+  it 'cannot log in with invalid credentials' do
+    user = User.create(
+      name: 'User One',
+      email: 'user_one@turing.edu',
+      password: 'password123',
+      password_confirmation: 'password123'
+    )
+
+    visit login_path
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: 'password987'
+    click_button 'Log In'
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content('Invalid email or password')
+  end
 end
