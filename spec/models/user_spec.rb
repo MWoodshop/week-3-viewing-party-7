14c363c61468b64ebcfc86b03010899b0aa8bdd6
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-05 16:06:43 -0400

    Test: updates model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email)}
-  end 
-  
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+
+    it 'should not store the password as plain text' do
+      user = User.create(
+        name: 'Meg',
+        email: 'meg@test.com',
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+
+      expect(user.attributes).to_not include('password')
+      expect(user.password_digest).to_not eq('password123')
+      expect(user.password_digest).to_not be_nil
+    end
+  end
 end
