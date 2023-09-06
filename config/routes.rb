81dae3846ec6558c25c6763df56ef69b98cb720b
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-06 18:14:30 -0400

    Feat: updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  # User Registration
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
+
+  # User Movies
   get '/users/:id/movies', to: 'movies#index', as: 'movies'
   get '/users/:user_id/movies/:id', to: 'movies#show', as: 'movie'
 
+  # User Login
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create'
+
+  # User Logout
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
   resources :users, only: :show
-  get '/login', to: 'users#login_form'
-  post '/login', to: 'users#login_user'
 end
